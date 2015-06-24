53746a58f7673ca37f12dbac8844b7009a0a7151
Ferruccio Irione <me@firo.it>
2015-06-24 10:17:28 +0200

    change tcp port from :3000 to :80

--- app.js
+++ app.js
@@ -60,6 +60,6 @@ io.on('connection', function(socket){
 
 
 // Express 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
